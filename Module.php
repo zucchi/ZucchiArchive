264f880cb366c6ad24eda8245ebad7dbe771adf1
Matt Cockayne <matt@zucchi.co.uk>
2013-03-07 14:13:14 +0000

    Initial Commi

---
+++ Module.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace ZucchiArchive;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ModuleManager\Feature\ServiceProviderInterface;
+
+class Module implements 
+    AutoloaderProviderInterface,
+    ConfigProviderInterface
+    
+{
+
+    public function onBootstrap($e)
+    {
+        $app = $e->getApplication();
+        $sm = $app->getServiceManager();
+
+        $serviceLoader = $sm->get('ServiceManager');
+        $serviceLoader->addInitializer(function ($instance) use ($sm) {
+            if (method_exists($instance, 'setServiceManager')) {
+                $instance->setServiceManager($sm);
+            }
+        });
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getConfig($env = null)
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+ZucchiArchive
+================
+
+A module to help facilitate archive management
+
+Installation
+------------
+
+From the root of your ZF2 Skeleton Application run
+
+    ./composer.phar require zucchi/archive
+
